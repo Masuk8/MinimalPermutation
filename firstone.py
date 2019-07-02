1a17daa300b79d80a5113ba3167fdee8f7b9cb88
Martin Sikora <martin.sikora8@gmail.com>
2019-07-02 23:21:33 +0200

    první stuff co dělám s dotama

---
+++ firstone.py
@@ -0,0 +1,16 @@
+min_dot =([1,2,3,4,5], [0,1,1,1,0] )
+min_dot1 = list(min_dot[0])
+min_dot2 = list(min_dot[1])
+
+
+print(min_dot1)
+print(min_dot2)
+
+min_dot2.sort()
+min_dot2.sort()
+
+print(min_dot2)
+
+
+
+"def min_dot(a, b):"
