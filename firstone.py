5f2ad13285618150e5873325c6cebc4ebb68633e
Martin Sikora <martin.sikora8@gmail.com>
2019-07-08 15:43:20 +0200

    první stuff co dělám s dotama
    tož někam jsem se posunul

--- firstone.py
+++ firstone.py
@@ -1,14 +1,10 @@
-min_dot = ([1, 2, 3, 4, 5], [0, 0, 1, 1, -4])
+min_dot = ([1, 3, 5], [4, -2, 1])
+
 a = list(min_dot[0])
 b = list(min_dot[1])
 
 
-print(a)
-print(b)
-
-
 def min_dot(a, b):
-    c = 0
     a.sort(reverse=True)
     b.sort()
     d = 0
@@ -17,21 +13,18 @@ def min_dot(a, b):
         d += c
         a.remove(max(a))
         b.remove(min(b))
-        print(d)
-        return d
+    print(d)
 
-
-"a.remove([0])"
 
 min_dot(a, b)
+# print(d)
+# a.remove([0])
+# a.remove(max(a))
 
-
-"a.remove(max(a))"
-
-"print(max(a))"
-"print(min(a))"
-
-"if no 0 and no negative, then it doesnt matter"
-"if no 0 and negative, then multiplyy 0 with highest numbers"
-"if is zero AND no negative, then multiply highest of a with lowest of b"
-"if is zero AND negative, then multiply negatives with highest numbers and rest of highest numbers with 0s"
+# print(max(a))
+# print(min(a))
+#
+# if no 0 and no negative, then it doesnt matter
+# if no 0 and negative, then multiplyy 0 with highest numbers
+# if is zero AND no negative, then multiply highest of a with lowest of b
+# if is zero AND negative, then multiply negatives with highest numbers and rest of highest numbers with 0s
